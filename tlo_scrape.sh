092a457df6877c80e6f340c0fc9bb26ffc7f7305
JurisHacker <JurisHacker@users.noreply.github.com>
2016-12-23 22:54:31 -0600

    adding email capability
    Added emailing the diff plus a couple more search terms

--- tlo_scrape.sh
+++ tlo_scrape.sh
@@ -1,8 +1,15 @@
-curl "http://www.capitol.state.tx.us/Search/TextSearchResults.aspx?CP=1&LegSess=85R&House=True&Senate=True&TypeB=True&TypeR=False&TypeJR=True&TypeCR=False&VerInt=True&VerHCR=True&VerEng=True&VerSCR=True&VerEnr=True&DocTypeB=True&DocTypeFN=False&DocTypeBA=False&DocTypeAM=False&Srch=simple&All=&Any=cyber*+cyber&Exact=&Exclude=&Custom=" | grep DocViewer > `date "+%Y%m%d"`.txt
+curl "http://www.capitol.state.tx.us/Search/TextSearchResults.aspx?CP=1&LegSess=85R&House=True&Senate=True&TypeB=True&TypeR=False&TypeJR=True&TypeCR=False&VerInt=True&VerHCR=True&VerEng=True&VerSCR=True&VerEnr=True&DocTypeB=True&DocTypeFN=False&DocTypeBA=False&DocTypeAM=False&Srch=simple&All=&Any=cyber*+cyber+privacy+"information+security"&Exact=&Exclude=&Custom=" | grep DocViewer > `date "+%Y%m%d"`.txt
 
 
 cat `date "+%Y%m%d.txt"` | awk 'BEGIN { FS = "[R,\)]"} ; {print $5}' > today.out
 
 diff yesterday.out today.out 
+
+# Email my the diff
+echo "  " > email.txt
+echo `cat diff.txt` >> email.txt
+echo "." >> email.txt
+cat email.txt | mail -s "TLO diff" add1@domain.com add2@domain2.com
+
 
 cp today.out yesterday.out
