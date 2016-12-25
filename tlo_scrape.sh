bdeb38079caf7782efb1ec5f3e48c8b03eed4781
JurisHacker <JurisHacker@users.noreply.github.com>
2016-12-24 21:50:00 -0600

    Fixing the awk statement
    Trying to make the awk statement a little cleaner

--- tlo_scrape.sh
+++ tlo_scrape.sh
@@ -1,9 +1,9 @@
 curl "http://www.capitol.state.tx.us/Search/TextSearchResults.aspx?CP=1&LegSess=85R&House=True&Senate=True&TypeB=True&TypeR=False&TypeJR=True&TypeCR=False&VerInt=True&VerHCR=True&VerEng=True&VerSCR=True&VerEnr=True&DocTypeB=True&DocTypeFN=False&DocTypeBA=False&DocTypeAM=False&Srch=simple&All=&Any=cyber*+cyber+privacy+"information+security"&Exact=&Exclude=&Custom=" | grep DocViewer > `date "+%Y%m%d"`.txt
 
 
-cat `date "+%Y%m%d.txt"` | awk 'BEGIN { FS = "[R,\)]"} ; {print $5}' > today.out
+cat `date "+%Y%m%d.txt"` | awk 'BEGIN { FS = "[)<]"} ; {print $3}' > today.out
 
-diff yesterday.out today.out 
+diff yesterday.out today.out > diff.txt
 
 # Email my the diff
 echo "  " > email.txt
