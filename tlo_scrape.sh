90a01f7225a16fb363c2eb82c87a63ad3fa171c3
JurisHacker <JurisHacker@users.noreply.github.com>
2016-12-29 18:03:59 -0600

    Making links work in email and stuff
    Made the email understand hyperlinks.  Also added in Author and Caption information.

--- tlo_scrape.sh
+++ tlo_scrape.sh
@@ -1,15 +1,22 @@
-curl "http://www.capitol.state.tx.us/Search/TextSearchResults.aspx?CP=1&LegSess=85R&House=True&Senate=True&TypeB=True&TypeR=False&TypeJR=True&TypeCR=False&VerInt=True&VerHCR=True&VerEng=True&VerSCR=True&VerEnr=True&DocTypeB=True&DocTypeFN=False&DocTypeBA=False&DocTypeAM=False&Srch=simple&All=&Any=cyber*+cyber+privacy+"information+security"&Exact=&Exclude=&Custom=" | grep DocViewer > `date "+%Y%m%d"`.txt
+#!/usr/pkg/bin/bash
 
+cd ~/scripts/tlo/tlo_scrapr/
 
-cat `date "+%Y%m%d.txt"` | awk 'BEGIN { FS = "[)<]"} ; {print $3}' > today.out
+curl "http://www.capitol.state.tx.us/Search/TextSearchResults.aspx?CP=1&LegSess=85R&House=True&Senate=True&TypeB=True&TypeR=False&TypeJR=True&TypeCR=False&VerInt=True&VerHCR=True&VerEng=True&VerSCR=True&VerEnr=True&DocTypeB=True&DocTypeFN=False&DocTypeBA=False&DocTypeAM=False&Srch=simple&All=&Any=cyber*+cyber+privacy+"information+security"&Exact=&Exclude=&Custom=" | grep -E 'DocViewer|Author|Caption' | grep -v 'style' | sed 's/\<\/span\>//' > `date "+%Y%m%d"`.txt
+
+cp `date "+%Y%m%d"`.txt input.txt
+sed -i.bak -e 's|a href="DocViewer.aspx|a href="http://www.capitol.state.tx.us/Search/DocViewer.aspx|g' input.txt
+cp input.txt today.out
 
 diff yesterday.out today.out > diff.txt
 
-# Email my the diff
+# Email me the diff
 echo "  " > email.txt
+echo "<html><head><title>New Bills</title></head><body>" >> email.txt
 echo `cat diff.txt` >> email.txt
+echo "<//body><//html>" >> email.txt
 echo "." >> email.txt
-cat email.txt | mail -s "TLO diff" add1@domain.com add2@domain2.com
+cat email.txt | mail -s"$(echo -e "TLO diff\nContent-Type: text/html")" user1@domain.com user2@domain.com
 
 
 cp today.out yesterday.out
